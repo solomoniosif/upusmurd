cf34bac954e462b5bc1bd7a4d9086f427102f3fc
iosif <solomoniosif@gmail.com>
2022-08-09 03:38:23 +0300

    added CSRF_TRUSTED_ORIGINS

--- upu_smurd/settings/local.py
+++ upu_smurd/settings/local.py
@@ -5,10 +5,11 @@ from .base import env
 DEBUG = True
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='django-insecure-7sk0&@-)wbj3*m7o6r-*pb@ji%i$8-u30=iri_29hd$gwbq+f_')
 
 ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = ['https://*.127.0.0.1', 'http://*.127.0.0.1', 'http://127.0.0.1:8080']
 
 EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
 EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
