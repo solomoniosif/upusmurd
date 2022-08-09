7c55295fccb6a186691e6e043277d7e5a3662ad2
iosif <solomoniosif@gmail.com>
2022-08-09 05:21:58 +0300

    removed unused import

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from djoser.serializers import UserCreateSerializer
-from phonenumber_field.serializerfields import PhoneNumberField
 from rest_framework import serializers
 
 User = get_user_model()
